88d0a9518e366ce9c19c2a3dbe7d7e66066bd2b1
Sergey Tyagushev <sergey_ty@mail.ru>
2023-02-06 07:40:27 +0300

    start async tcp

---
+++ WindowsFormsApp4/modbusTCP.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace WindowsFormsApp9
+{
+    class ModbusTCPserver
+    {
+        Socket socket;
+
+        public ModbusTCPserver()
+        {
+
+           
+
+
+        }
+
+        public async void Connect() {
+
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                var port = 8888;
+                var url = "localhodt";
+                // пытаемся подключиться используя URL-адрес и 
+                await socket.ConnectAsync(url, port);
+                Console.WriteLine($"Подключение к {url} установлено");
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine($"Не удалось установить подключение");
+            }
+        }
+
+        public async void SlavePollAsync(int delay)
+        {
+            Debug.WriteLine("-->");
+            while (true)
+            {
+                byte[] data = { 0x01, 0x06, 0, 0, 0 , 9 };
+                int bytesSent = await socket.SendT;
+
+                await Task.Delay(delay);
+            }
+        }
+    }
+}
